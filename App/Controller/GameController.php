8c97b7b649124becee8a0d249ff9c118e9fd1ae1
LuizJarduli <luizjarduli@gmail.com>
2020-12-27 23:40:57 -0300

    Criando os métodos no Controller

--- App/Controller/GameController.php
+++ App/Controller/GameController.php
@@ -1,11 +1,36 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Game;
 
 class GameController{
 
-    function __construct()
+    //POST - Cria um novo game
+    function create($data = null){
+        return json_encode(["name" => "create"]);
+    }
+
+    //PUT - Altera um game
+    function update($id = 0, $data = null)
     {
-        echo "Chamou a GameController";
+        return json_encode(["name" => "update"]);
+    }
+
+    //GET - Retorna um game pelo ID
+    function readById($id = 0)
+    {
+        return json_encode(["name" => "readById"]);
+    }
+
+    //DELETE - Deleta uma game pelo ID
+    function delete($id = 0)
+    {
+        return json_encode(["name" => "delete"]);
+    }
+
+    //GET - Lê todos os registros
+    function readAll()
+    {
+        return json_encode(["name" => "readAll"]);
     }
 }
