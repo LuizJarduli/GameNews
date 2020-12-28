34d16749fbfb2d0ea555c52cf6a27bff04d80adc
LuizJarduli <luizjarduli@gmail.com>
2020-12-27 21:37:42 -0300

    Incluindo HEADER

--- Public/api/index.php
+++ Public/api/index.php
@@ -1,7 +1,20 @@
 <?php
 
     require_once("../../vendor/autoload.php");
-    use App\Controller\GameController;
 
-    $gameController = new GameController();
+    //HEADER
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    //HEADER
+
+
+    $controller = null; // controller (EX: /game)
+    $param = null; // parâmetro (EX: /game/:id)
+    $data = null;
+    $method = $_SERVER["REQUEST_METHOD"]; //POST, PUT, DELETE and GET
+    //echo $method;
+    
 ?>
